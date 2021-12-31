1487ad501e3e768866f8670cd09a8f24974a4012
Aman_Yadav <19BCS2461@gmail.com>
2021-12-31 13:27:51 +0530

    add object prototype

--- js/tut28.js
+++ js/tut28.js
@@ -1,1 +1,25 @@
 console.log('Object prototype');
+
+//object literal
+
+let obj={
+    name:"Aman",
+    Branch:"CSE",
+    college:"Chandigarh University"
+    
+}
+
+function Obj(Name,branch,College){
+    this.name=Name,
+    this.Branch=branch,
+    this.college=College
+}
+Obj.prototype.getName = function(){
+    return this.name;
+}
+
+Obj.prototype.setName = function(newName){
+    this.name=newName;
+}
+let obj2= new Obj("Amit","General CSE","Private College");
+console.log(obj2);
